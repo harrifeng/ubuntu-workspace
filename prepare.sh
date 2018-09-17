03a5a87a2bed2cda76dbe620cf2b7eb45f6db983
Harri Feng <harrifeng@gmail.com>
2018-09-17 14:13:44 +0800

    Init

---
+++ prepare.sh
@@ -0,0 +1,24 @@
+echo "DNS1=8.8.8.8" | sudo tee -a /etc/network/interface
+mkdir -p /home/vagrant/github
+
+DOT_FILES='/home/vagrant/github/dotfiles'
+if [ ! -d "$DOT_FILES" ]; then
+    git clone https://github.com/harrifeng/dotfiles.git ${DOT_FILES}
+    bash ${DOT_FILES}/run.sh
+fi
+
+chown -R vagrant:vagrant /home/vagrant/github
+
+echo "
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+" | sudo tee /etc/apt/sources.list
+
+
+echo "<----------finishing provision--------------->"
