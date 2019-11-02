79792873e21b868873ddfd1af1f175187235075a
harri feng <harrifeng@gmail.com>
2019-11-02 21:50:50 +0800

    update prepare

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,3 @@
-echo "DNS1=8.8.8.8" | sudo tee -a /etc/network/interfaces
-mkdir -p /home/vagrant/github
-
-DOT_FILES='/home/vagrant/github/dotfiles'
-if [ ! -d "$DOT_FILES" ]; then
-    git clone https://github.com/harrifeng/dotfiles.git ${DOT_FILES}
-    bash ${DOT_FILES}/run.sh
-fi
-
-chown -R vagrant:vagrant /home/vagrant/github
-
 echo "
 deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
 deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
@@ -23,6 +12,6 @@ deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted unive
 " | sudo tee /etc/apt/sources.list
 
 sudo apt update
-sudo apt install -y emacs git silversearcher-ag lrzsz python-pip python3-distutils python3-dev tmux libmysqlclient-dev
+sudo apt install -y emacs git silversearcher-ag lrzsz python-pip python3-distutils python3-dev tmux libmysqlclient-dev nload
 
 echo "<----------finishing provision--------------->"
