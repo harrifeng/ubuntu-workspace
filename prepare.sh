102cfa6b83cc7d67dd1a173d9e9e18c6347d3fb8
Haoran Feng <harrifeng@gmail.com>
2022-12-19 13:33:36 +0800

    2022-12-19-13-33

--- prepare.sh
+++ prepare.sh
@@ -1,17 +1,4 @@
-echo "
-deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
-" | sudo tee /etc/apt/sources.list
-
 sudo apt update
-sudo apt install -y emacs git silversearcher-ag lrzsz python-pip python3-distutils python3-dev tmux libmysqlclient-dev nload
+sudo apt install -y git silversearcher-ag nload
 
 echo "<----------finishing provision--------------->"
